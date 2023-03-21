d7555c1fa5a2606e79bdfbf2eb6c49a06b1cf0c9
saharbagheri <54686930+saharbagheri@users.noreply.github.com>
2023-03-21 16:30:51 -0600

    species assignmnet is kept

--- utils/scripts/dada2/RDPtaxa.R
+++ utils/scripts/dada2/RDPtaxa.R
@@ -1,31 +1,15 @@
+
 suppressMessages(library(dada2))
 suppressMessages(library(Biostrings))
-#suppressMessages(library(DECIPHER))
 
-
-#packageVersion("DECIPHER")
 
 seqtab= readRDS(snakemake@input[['seqtab']]) # seqtab
-
-print(snakemake)
-
-dna <- DNAStringSet(getSequences(seqtab)) # Create a DNAStringSet from the ASVs
-
-#load(snakemake@input[['ref']]) #trainingSet
-
-print(snakemake@input[['species']])
-print(snakemake@input[['ref']])
-
 
 set.seed(snakemake@config[['seed']]) # Initialize random number generator for reproducibility
 
 taxtab <- assignTaxonomy(seqtab, refFasta = snakemake@input[['ref']],tryRC=TRUE,multithread=snakemake@threads,outputBootstraps = T)
-
-print( colnames(taxtab$tax))
-write.table(taxtab$tax,snakemake@output[['taxonomy']],sep='\t')
 saveRDS(taxtab,file=snakemake@output[['rds_bootstrap']])
 
+taxtab<-addSpecies(taxtab$tax, refFasta=snakemake@input[['species']],tryRC=TRUE,allowMultiple = TRUE)
 
-
-
-
+write.table(taxtab,snakemake@output[['taxonomy']],sep='\t')
