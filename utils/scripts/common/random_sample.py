c0f807083bb2b5589b7cbd2204a47a6e80156d3c
saharbagheri <54686930+saharbagheri@users.noreply.github.com>
2023-03-23 18:17:43 -0600

    selecting random samples for plotting

---
+++ utils/scripts/common/random_sample.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+list_files = pd.read_csv(snakemake.input[0], sep="\t", index_col=0)
+print(list_files)
+subset = list_files[list_files["R1"].str.contains(snakemake.config["excluded_samples"]) == False]
+print(subset)
+random_samples = subset.sample(n=8)
+print(random_samples)
+random_samples.to_csv(snakemake.output[0], index=True,columns=[], header=False, line_terminator="\n")
+
