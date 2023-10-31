c42aa45d28ef3066a468aac3e32769b016a62edf
saharbagheri <54686930+saharbagheri@users.noreply.github.com>
2023-10-31 12:43:05 -0600

    updating random_sample.py

--- utils/scripts/common/random_sample.py
+++ utils/scripts/common/random_sample.py
@@ -4,7 +4,7 @@ list_files = pd.read_csv(snakemake.input[0], sep="\t", index_col=0)
 print(list_files)
 subset = list_files[list_files["R1"].str.contains(snakemake.config["excluded_samples"]) == False]
 print(subset)
-random_samples = subset.sample(n=8)
+random_samples = subset.sample(n=snakemake.config["random_n"])
 print(random_samples)
 random_samples.to_csv(snakemake.output[0], index=True,columns=[], header=False)
 
