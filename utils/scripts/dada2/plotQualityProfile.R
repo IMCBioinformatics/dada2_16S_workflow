62030c19339e6f9d2b39448f86de5a19998f27a3
saharbagheri <54686930+saharbagheri@users.noreply.github.com>
2023-12-14 15:57:34 -0700

    disregarding samples with zero reads

--- utils/scripts/dada2/plotQualityProfile.R
+++ utils/scripts/dada2/plotQualityProfile.R
@@ -1,17 +1,57 @@
 suppressMessages(library(dada2))
 suppressMessages(library(ggplot2))
 suppressMessages(library(gridExtra))
+suppressMessages(library(ShortRead))
+
+filtFs = snakemake@input[['R1']]
+filtRs = snakemake@input[['R2']]
 
 
+exists <- file.exists(filtFs) & file.exists(filtRs)
+filtFs <- filtFs[exists]
+filtRs <- filtRs[exists]
 
-p_F<- plotQualityProfile(snakemake@input$R1,n=1e5,aggregate=T) + theme_classic(base_size=6)
+# Create a function to count reads in a FASTQ file
+count_reads <- function(file_path) {
+  reads <- readFastq(file_path)
+  return(length(reads))
+}
+
+
+# Iterate through the list of FASTQ files and count reads
+valid_filtFs <- character(0)  # To store valid file paths
+
+for (file_path in filtFs) {
+  read_count <- count_reads(file_path)
+  if (read_count > 0) {
+    valid_filtFs <- c(valid_filtFs, file_path)
+  } else {
+    print(paste("File:", file_path, "has zero reads and will be disregarded.\n"))
+  }
+}
+
+
+# Iterate through the list of FASTQ files and count reads
+valid_filtRs <- character(0)  # To store valid file paths
+
+for (file_path in filtRs) {
+  read_count <- count_reads(file_path)
+  if (read_count > 0) {
+    valid_filtRs <- c(valid_filtRs, file_path)
+  } else {
+    print(paste("File:", file_path, "has zero reads and will be disregarded.\n"))
+  }
+}
+
+
+p_F<- plotQualityProfile(valid_filtFs,n=1e5,aggregate=T) + theme_classic(base_size=6)
 print("Out of plotQualityProfile R1")
 
 
 ggsave(snakemake@output$R1,p_F)
 
 
-p_R<- plotQualityProfile(snakemake@input$R2,n=1e5,aggregate=T) + theme_classic(base_size=6)
+p_R<- plotQualityProfile(valid_filtRs,n=1e5,aggregate=T) + theme_classic(base_size=6)
 
 print("Out of plotQualityProfile R2")
 
