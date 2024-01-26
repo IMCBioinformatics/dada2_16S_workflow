13eb2fc4c197fbe086c74b686c7f82ed19fd1d5e
saharbagheri <54686930+saharbagheri@users.noreply.github.com>
2024-01-26 15:39:47 -0700

    updating annotation_output_dada2.R

--- utils/scripts/dada2/annotation_output_dada2.R
+++ utils/scripts/dada2/annotation_output_dada2.R
@@ -13,10 +13,6 @@ taxa_Silva<-read.table(paste0(snakemake@config[["output_dir"]],"/taxonomy/Silva_
 taxa_Silva<-cbind(taxa_Silva,rownames(taxa_Silva))
 colnames(taxa_Silva)[8]<-"asv_seq"
 
-taxa_URE<-read.table(paste0(snakemake@config[["output_dir"]],"/taxonomy/URE_RDP.tsv"))
-taxa_URE<-cbind(taxa_URE,rownames(taxa_URE))
-colnames(taxa_URE)[8]<-"asv_seq"
-
 seqtab<-readRDS(snakemake@input[["seqs"]])
 seqtab2<-t(seqtab)
 
@@ -39,18 +35,16 @@ colnames(df)<-c("asv_num", "asv_seq","asv_len")
 df1<-left_join(df,taxa_GTDB,by="asv_seq")
 df2<-left_join(df1,taxa_RDP,by="asv_seq")
 df3<-left_join(df2,taxa_Silva,by="asv_seq")
-df4<-left_join(df3,taxa_URE,by="asv_seq")
-df5<-left_join(df4,seqtab4,by="asv_seq")
+df4<-left_join(df3,seqtab4,by="asv_seq")
   
-colnames(df5)[1:31]<-c("asv_num", "asv_seq", "asv_len",	
+colnames(df4)[1:24]<-c("asv_num", "asv_seq", "asv_len",	
                          "kingdom_gtdb", "phylum_gtdb",	"class_gtdb",	"order_gtdb",	
                          "family_gtdb",	"genus_gtdb",	"species_gtdb",	"kingdom_rdp",	
                          "phylum_rdp",	"class_rdp",	"order_rdp",	"family_rdp",	
                          "genus_rdp",	"species_rdp",	"kingdom_silva",	"phylum_silva",	
                          "class_silva",	"order_silva",	"family_silva",	"genus_silva",	
-                         "species_silva","kingdom_URE",	"phylum_URE",	"class_URE",	
-                         "order_URE",	"family_URE",	"genus_URE",	"species_URE")
+                         "species_silva")
     
 
-colnames(df5) <- gsub("^X", "", colnames(df5))
-write.csv(df5,snakemake@output[["table"]],row.names = F)
+colnames(df4) <- gsub("^X", "", colnames(df4))
+write.csv(df4,snakemake@output[["table"]],row.names = F)
