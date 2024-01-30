44836bbb3f3b44cd873d120369a38a25a839c70a
saharbagheri <54686930+saharbagheri@users.noreply.github.com>
2024-01-29 17:03:54 -0700

    updating annotation_output_dada2.R

--- utils/scripts/dada2/annotation_output_dada2.R
+++ utils/scripts/dada2/annotation_output_dada2.R
@@ -27,7 +27,7 @@ colnames(seqtab4)[1]<-"asv_seq"
 
 ##Genering ASV_number,sequences and length columns
 df<-data.frame(cbind(paste0("ASV_",seq_along(1:nrow(taxa_GTDB))),rownames(taxa_GTDB),length<-nchar(rownames(taxa_GTDB))))
-colnames(df)<-c("asv_num", "asv_seq","asv_len")
+colnames(df)<-c("asv_id", "asv_seq","asv_len")
 
 
 #joining all taxonoy files ands seq table by ASV seqs
@@ -37,7 +37,7 @@ df2<-left_join(df1,taxa_RDP,by="asv_seq")
 df3<-left_join(df2,taxa_Silva,by="asv_seq")
 df4<-left_join(df3,seqtab4,by="asv_seq")
   
-colnames(df4)[1:24]<-c("asv_num", "asv_seq", "asv_len",	
+colnames(df4)[1:24]<-c("asv_id", "asv_seq", "asv_len",	
                          "kingdom_gtdb", "phylum_gtdb",	"class_gtdb",	"order_gtdb",	
                          "family_gtdb",	"genus_gtdb",	"species_gtdb",	"kingdom_rdp",	
                          "phylum_rdp",	"class_rdp",	"order_rdp",	"family_rdp",	
