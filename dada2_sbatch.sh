46f30e9d4ccdc5f8053615e6e1ecef0e0a60239a
saharbagheri <sahar.bagheri1@ucalgary.ca>
2026-05-29 13:17:33 -0600

    Update dada2_sbatch.sh

--- dada2_sbatch.sh
+++ dada2_sbatch.sh
@@ -20,7 +20,7 @@ snakemake --unlock
 snakemake --rerun-triggers mtime --latency-wait 60 --rerun-incomplete  --cluster-config cluster.json --cluster 'sbatch --partition={cluster.partition} --cpus-per-task={cluster.cpus-per-task} --nodes={cluster.nodes} --ntasks={cluster.ntasks} --time={cluster.time} --mem={cluster.mem} --output={cluster.output} --error={cluster.error}' --jobs $num_jobs --use-conda &>> $log_dir/$log_file
 
 output_dir=$(grep "output_dir" < config.yaml | cut -d ' ' -f2 | sed 's/"//g')
-list_files=$(grep "sampletable" < config.yaml | cut -d ' ' -f2 | sed 's/"//g')
+list_files=$(grep "list_files" < config.yaml | cut -d ' ' -f2 | sed 's/"//g')
 
 #Copying all snakemake/log files of the run in the output folder
 snakemake_file_dir="${output_dir}/snakemake_files"
